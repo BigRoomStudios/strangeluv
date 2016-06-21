f6e26ae288d5f00b5551f1dddcd13d3e3d2bb436
Devin Ivy <devin@bigroomstudios.com>
2016-06-21 13:21:34 -0400

    Rename files for simplicity and deduping

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,16 @@
-module.exports = require('./Header');
+const React = require('react');
+const Router = require('react-router');
+const Classes = require('./styles.scss');
+
+module.exports = () => (
+  <div>
+    <h1>React Redux Starter Kit</h1>
+    <Router.IndexLink to='/' activeClassName={Classes.activeRoute}>
+      Home
+    </Router.IndexLink>
+    {' · '}
+    <Router.Link to='/counter' activeClassName={Classes.activeRoute}>
+      Counter
+    </Router.Link>
+  </div>
+);
--- src/layouts/CoreLayout/index.js
+++ src/layouts/CoreLayout/index.js
@@ -1,1 +1,19 @@
-module.exports = require('./CoreLayout');
+const React = require('react');
+const Header = require('../../components/Header');
+const Classes = require('./styles.scss');
+
+// Pull global styles
+require('../../styles/core.scss');
+
+module.exports = exports = ({ children }) => (
+  <div className='container text-center'>
+    <Header />
+    <div className={Classes.mainContainer}>
+      {children}
+    </div>
+  </div>
+);
+
+exports.propTypes = {
+  children: React.PropTypes.element.isRequired
+};
