e65004a87e46dc5d04ca37d0529022fc19a0f7ed
Devin Ivy <devin@bigroomstudios.com>
2016-06-22 16:25:48 -0400

    No async/await in compiler script

--- bin/compile.js
+++ bin/compile.js
@@ -6,20 +6,21 @@ const Debug = require('debug')('app:bin:compile');
 
 const paths = Config.utils_paths;
 
-(async function () {
+Debug('Run compiler');
 
-    try {
-        Debug('Run compiler');
-        const stats = await WebpackCompiler(WebpackConfig);
-        if (stats.warnings.length && Config.compiler_fail_on_warning) {
-            Debug('Config set to fail on warning, exiting with status code "1".');
-            process.exit(1);
-        }
-        Debug('Copy static assets to dist folder.');
-        Fs.copySync(paths.client('static'), paths.dist());
-    }
-    catch (e) {
-        Debug('Compiler encountered an error.', e);
+WebpackCompiler(WebpackConfig)
+.then((stats) => {
+
+    if (stats.warnings.length && Config.compiler_fail_on_warning) {
+        Debug('Config set to fail on warning, exiting with status code "1".');
         process.exit(1);
     }
-})();
+
+    Debug('Copy static assets to dist folder.');
+    Fs.copySync(paths.client('static'), paths.dist());
+})
+.catch((err) => {
+
+    Debug('Compiler encountered an error.', err);
+    process.exit(1);
+});
