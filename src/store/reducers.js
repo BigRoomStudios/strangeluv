052af237768125183467482b84af8f6e71b516ba
Devin Ivy <devin@bigroomstudios.com>
2016-06-23 19:11:11 -0400

    Make the router a normal reducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,11 +1,9 @@
 const Redux = require('redux');
-const RouterReducer = require('react-router-redux').routerReducer;
 const Wiring = require('../wiring');
 
 exports.makeRoot = (asyncReducers) => {
 
     return Redux.combineReducers({
-        router: RouterReducer,
         ...Wiring.reducers(), // Everything in reducers/
         ...asyncReducers
     });
