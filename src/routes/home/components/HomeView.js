835bb7764b28741efddd5fd3d47bff5d1d24f3f6
Devin Ivy <devin@bigroomstudios.com>
2016-06-29 10:39:30 -0400

    Give the HomeView moon-phase related props and include the MoonPhase component

--- src/routes/home/components/HomeView.js
+++ src/routes/home/components/HomeView.js
@@ -1,16 +1,50 @@
 const React = require('react');
 const DuckImage = require('../assets/duck.jpg');
 const Classes = require('./HomeView.scss');
+const MoonPhase = require('./MoonPhase');
 
-module.exports = () => (
+module.exports = class extends React.Component {
 
-    <div>
-        <h4>Welcome!</h4>
-        <img
-            alt='This is a duck, because Redux!'
-            className={Classes.duck}
-            src={DuckImage}
-        />
-    </div>
+    static propTypes = {
+        loadMoon: React.PropTypes.func.isRequired,
+        moonId: React.PropTypes.number,
+        isLoading: React.PropTypes.bool,
+        errorMessage: React.PropTypes.string
+    }
 
-);
+    // Indicates that the component is going to be rendered onto the page
+    componentWillMount() {
+
+        // Intend to load the moon phase only if we don't already have it
+        if (!this.props.moonId) {
+            this.props.loadMoon();
+        }
+    }
+
+    render() {
+
+        const { errorMessage, moonId, isLoading } = this.props;
+
+        return (
+            <div>
+                <h4>Welcome!</h4>
+                <img
+                    alt='This is a duck, because Redux!'
+                    className={Classes.duck}
+                    src={DuckImage}
+                />
+
+                {/* Show error message if something went awry */}
+                {errorMessage &&
+                    <div>{errorMessage}</div>}
+
+                {/* Show loader if we're waiting to get the moon phase */}
+                {isLoading &&
+                    <div>Loading...</div>}
+
+                {/* Display moon phase image or a placeholder based upon the moonId */}
+                <MoonPhase moonId={moonId} />
+            </div>
+        );
+    }
+};
