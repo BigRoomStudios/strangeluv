cde4331a039313f6df2808470363017e6bace5c9
Devin Ivy <devin@bigroomstudios.com>
2016-06-29 10:28:52 -0400

    Create a reducer to process moon-phase actions into moon-phase state

--- src/reducers/moon-phase.js
+++ src/reducers/moon-phase.js
@@ -1,7 +1,12 @@
 const MoonPhaseTypes = require('../action-types/moon-phase');
 
 const internals = {
-    initial: () => ({})
+    // We want to keep track of...
+    initial: () => ({
+        isLoading: false, // whether we're waiting to know the moon phase,
+        moonId: null,     // the moon phase id once it is fetched,
+        error: null       // any error states that we entered while fetching the moon phase
+    })
 };
 
 module.exports = (state, action) => {
@@ -12,8 +17,22 @@ module.exports = (state, action) => {
     const payload = action.payload;
 
     switch (type) {
-        // case MoonPhaseTypes.ACTION_NAME:
-        //     return state;
+        case MoonPhaseTypes.MOON_LOAD_BEGIN:
+            return Object.assign({}, state, {
+                isLoading: true
+            });
+        case MoonPhaseTypes.MOON_LOADED:
+            return Object.assign({}, state, {
+                isLoading: false,
+                moonId: payload.Index,
+                error: null
+            });
+        case MoonPhaseTypes.MOON_LOAD_ERROR:
+            return Object.assign({}, state, {
+                isLoading: false,
+                moonId: null,
+                error: payload
+            });
         default:
             return state;
     }
