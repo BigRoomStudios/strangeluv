7dd60fb16268b8f522b16969f330d34d10400598
Devin Ivy <devin@bigroomstudios.com>
2016-06-29 10:29:40 -0400

    Make a dumb component that displays a moon phase image

---
+++ src/routes/home/components/MoonPhase.js
@@ -0,0 +1,31 @@
+const React = require('react');
+
+const internals = {};
+
+const MoonPhase = ({ moonId }) => (
+    // All we need to do is display an image
+    // of the moon phase, otherwise a placeholder
+    <img src={internals.src(moonId)} style={internals.style} />
+);
+
+// Make sure we don't get any weird moonIds
+MoonPhase.propTypes = {
+    moonId: React.PropTypes.number
+};
+
+module.exports = MoonPhase;
+
+internals.style = {
+    maxWidth: '100px'
+};
+
+internals.src = (moonId) => {
+
+    // If no moonId, show a placeholder image
+    if (!moonId && moonId !== 0) {
+        return 'http://images.zap2it.com/assets/p406044_b_h3_aa/if-we-had-no-moon.jpg';
+    }
+
+    // If we do have a good moonId, show what the moon phase looks like
+    return `http://www.farmsense.net/demos/images/moonphases/${moonId}.png`;
+};
