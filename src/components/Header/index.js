1d43ce80f3c729eb864739ce76d13b624a1eef81
Geoff <keating.geoff@gmail.com>
2018-03-22 08:31:22 -0400

    Adding Material UI to project, setting up a theme, adding a basic button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,17 +2,23 @@ const React = require('react');
 const NavLink = require('react-router-dom').NavLink;
 const Classes = require('./styles.scss');
 
+const Button = require('material-ui/Button').default;
+
 module.exports = () => (
 
     <div>
         <h1>Strangeluv</h1>
+
+        <Button color='secondary'>Secondary Button</Button> {/* Basic button */}
+
+        <br />
+
         <NavLink exact to='/' activeClassName={Classes.activeRoute}>
-            Home
+          test
         </NavLink>
         {' · '}
         <NavLink to='/counter' activeClassName={Classes.activeRoute}>
             Counter
         </NavLink>
     </div>
-
 );
