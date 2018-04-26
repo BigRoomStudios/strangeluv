746a43a5f7d0b4b7ed4fc9186057d2676abcc334
WilliamSWoodruff <williamswoodruff@gmail.com>
2018-04-26 14:28:35 -0400

    Couple changes to styled-components example

--- src/components/material/button-styledComponent.js
+++ src/components/material/button-styledComponent.js
@@ -6,19 +6,30 @@ const Link = require('react-router-dom').Link;
 const Colors = require('styles/colors');
 // https://github.com/mui-org/material-ui/blob/28c1f7ab5d60d757c5fbdb8dd11194b3ca0141d2/docs/src/pages/guides/interoperability.md#styled-components.
 
-const SuperCoolButton = Styled((props) => <Button {...props} classes={{ label: 'label' }} />)`
+const internals = {};
+
+// Just some random color function I found online
+internals.randomColor = () => '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1,6);
+
+const SuperCoolButton = Styled(Button)
+    .attrs({ // Showing that you can add props via the `.attrs` func
+        classes: { label: 'label' }
+    })`
     background: linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%);
     border-radius: 3px;
-    border: 0;
     height: 48px;
     padding: 0 30px;
-    && { // hit the root element
-        box-shadow: 0 3px 5px 2px rgba(255, 105, 135, .30);
-    },
-    & .label {
+
+    // Target the root component
+    && {
+        border: ${(props) => '5px solid ' + props.randomColor};
+    }
+
+    box-shadow: 0 3px 5px 2px rgba(255, 105, 135, 0.3);
+    .label {
         color: white;
-    },
-    &:hover {
+    }
+    :hover {
         background: linear-gradient(45deg, #FE6B8B 30%, ${Colors.yellow} 90%);
     }
 `;
@@ -28,7 +39,7 @@ const ButtonComponent = (props) => (
     <SuperCoolButton
         // if we don't receive a path, we need to skip this aspect
         component={props.path ? Link : null}
-        to={props.path || null}
+        randomColor={internals.randomColor()}
     >
         {props.title}
     </SuperCoolButton>
