d259b7f0a7767474378697250de3604b04b94cd9
wswoodruff <williamswoodruff@gmail.com>
2019-09-14 12:10:23 -0400

    Remove incomplete double action

--- src/middle-end/mods/counter/action-types.js
+++ src/middle-end/mods/counter/action-types.js
@@ -2,6 +2,5 @@
 const M = require('middle-end');
 
 module.exports = M.createTypes({
-    INCREMENT: M.type.simple,
-    DOUBLE: M.type.async
+    INCREMENT: M.type.simple
 });
--- src/middle-end/mods/counter/actions.js
+++ src/middle-end/mods/counter/actions.js
@@ -1,16 +1,8 @@
 
 const M = require('middle-end');
 
-const { INCREMENT, DOUBLE } = require('./action-types');
+const { INCREMENT } = require('./action-types');
 
 module.exports = {
-    increment: M.createAction(INCREMENT),
-    double: M.createAction(DOUBLE, {
-        handler: async () => {
-
-            await new Promise((res) => setTimeout(res, 200));
-
-            
-        }
-    })
+    increment: M.createAction(INCREMENT)
 };
