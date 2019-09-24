8d015d5fb8fbdba55bad0b0638f9c7cd2df461e3
Devin Ivy <devin@bigroomstudios.com>
2019-09-24 11:39:16 -0400

    Apply global style from polished

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ const HotLoader = require('react-hot-loader/root');
 const ReactRedux = require('react-redux');
 const { ConnectedRouter } = require('connected-react-router');
 const StrangeRouter = require('strange-router');
+const GlobalStyle = require('./components/GlobalStyle');
 const Routes = require('./routes');
-
-// const GlobalStyles = require('./components/GlobalStyles');
 
 module.exports = ({ store, Router = ConnectedRouter, ...routerProps }) => {
 
     return <ReactRedux.Provider store={store}>
-        {/* <GlobalStyles /> */}
+        <GlobalStyle />
         <Router {...routerProps}>
             <StrangeRouter.Routes routes={Routes} />
         </Router>
