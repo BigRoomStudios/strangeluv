4207e9669f329bbe8727d0fbec0d91e53d46f5f7
Devin Ivy <devin@bigroomstudios.com>
2019-09-24 22:22:44 -0400

    Tabs to spaces.

--- webpack.config.js
+++ webpack.config.js
@@ -32,101 +32,101 @@ Dotenv.config();
 const isEnvProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-	mode: isEnvProduction ? 'production' : 'development',	// TODO
-	entry: './src/index.js',
-	output: {
-		filename: '[name].[hash].js', // TODO HMR vs prod
-		path: Path.resolve(__dirname, 'dist'),
-		// Allows clickable/openable stacktraces in development
-		devtoolModuleFilenameTemplate: isEnvProduction ?
-			(info) => Path.relative(Path.resolve(__dirname, 'src'), info.absoluteResourcePath).replace(/\\/g, '/') :
-			(info) => Path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')
-	},
-	resolve: {
-		alias: {
-		  'react-dom': '@hot-loader/react-dom'
-		}
-	},
-	plugins: [
-		new Webpack.ProgressPlugin(),
-		new Webpack.DefinePlugin({
-			'process.env': [
-				'NODE_ENV'
-			].reduce((collect, key) => ({
-				...collect,
-				[key]: JSON.stringify(process.env[key])
-			}), {})
-		}),
-		new ErrorOverlayPlugin(),
-		new HtmlWebpackPlugin({
-			template: 'src/index.html'
-		})
-	],
-	devtool: 'cheap-module-source-map',	// Not 'eval' for overlay
-	module: {
-		rules: [
-			{
-				oneOf: [
-					{
-						test: /\.(js|mjs|jsx|ts|tsx)$/,
-						exclude: /node_modules/,
-						loader: 'babel-loader',
-						options: {
-							sourceType: 'unambiguous',
-							customize: require.resolve('babel-preset-react-app/webpack-overrides'),
-							cacheDirectory: true,
-							cacheCompression: false,
-							compact: isEnvProduction
-						}
-					},
-					{
-						test: /\.(js|mjs)$/,
-						exclude: /@babel(?:\/|\\{1,2})runtime/,
-						loader: 'babel-loader',
-						options: {
-							presets: [
-								[
-									require.resolve('babel-preset-react-app/dependencies'),
-									{ helpers: true }
-								]
-							],
-							babelrc: false,
-							configFile: false,
-							compact: false,
-							cacheDirectory: true,
-							cacheCompression: false,
-							// If an error happens in a package, it's possible to be
-							// because it was compiled. Thus, we don't want the browser
-							// debugger to show the original code. Instead, the code
-							// being evaluated would be much more helpful.
-							sourceMaps: false
-						}
-					},
-					{
-						test: /\.(png|svg|jpg|gif)$/,
-						loader: 'file-loader'
-					}
-				]
-			}
-		]
-	},
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					priority: -10,
-					test: /[\\/]node_modules[\\/]/
-				}
-			},
-			chunks: 'async',
-			minChunks: 1,
-			minSize: 30000,
-			name: true
-		}
-	},
-	devServer: {
-		open: true,
-		hot: true,
-		historyApiFallback: true
-	}
+    mode: isEnvProduction ? 'production' : 'development',    // TODO
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[hash].js', // TODO HMR vs prod
+        path: Path.resolve(__dirname, 'dist'),
+        // Allows clickable/openable stacktraces in development
+        devtoolModuleFilenameTemplate: isEnvProduction ?
+            (info) => Path.relative(Path.resolve(__dirname, 'src'), info.absoluteResourcePath).replace(/\\/g, '/') :
+            (info) => Path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')
+    },
+    resolve: {
+        alias: {
+          'react-dom': '@hot-loader/react-dom'
+        }
+    },
+    plugins: [
+        new Webpack.ProgressPlugin(),
+        new Webpack.DefinePlugin({
+            'process.env': [
+                'NODE_ENV'
+            ].reduce((collect, key) => ({
+                ...collect,
+                [key]: JSON.stringify(process.env[key])
+            }), {})
+        }),
+        new ErrorOverlayPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ],
+    devtool: 'cheap-module-source-map',    // Not 'eval' for overlay
+    module: {
+        rules: [
+            {
+                oneOf: [
+                    {
+                        test: /\.(js|mjs|jsx|ts|tsx)$/,
+                        exclude: /node_modules/,
+                        loader: 'babel-loader',
+                        options: {
+                            sourceType: 'unambiguous',
+                            customize: require.resolve('babel-preset-react-app/webpack-overrides'),
+                            cacheDirectory: true,
+                            cacheCompression: false,
+                            compact: isEnvProduction
+                        }
+                    },
+                    {
+                        test: /\.(js|mjs)$/,
+                        exclude: /@babel(?:\/|\\{1,2})runtime/,
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    require.resolve('babel-preset-react-app/dependencies'),
+                                    { helpers: true }
+                                ]
+                            ],
+                            babelrc: false,
+                            configFile: false,
+                            compact: false,
+                            cacheDirectory: true,
+                            cacheCompression: false,
+                            // If an error happens in a package, it's possible to be
+                            // because it was compiled. Thus, we don't want the browser
+                            // debugger to show the original code. Instead, the code
+                            // being evaluated would be much more helpful.
+                            sourceMaps: false
+                        }
+                    },
+                    {
+                        test: /\.(png|svg|jpg|gif)$/,
+                        loader: 'file-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendors: {
+                    priority: -10,
+                    test: /[\\/]node_modules[\\/]/
+                }
+            },
+            chunks: 'async',
+            minChunks: 1,
+            minSize: 30000,
+            name: true
+        }
+    },
+    devServer: {
+        open: true,
+        hot: true,
+        historyApiFallback: true
+    }
 };
