bc6c5bdd15f9157adceb891c6244edb0387111de
Devin Ivy <devin@bigroomstudios.com>
2019-09-30 18:16:02 -0400

    Make middle-end usable as non-singleton.

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,17 @@
 const React = require('react');
 const Testing = require('@testing-library/react');
-const M = require('./middle-end');
+const MiddleEnd = require('strange-middle-end');
+const Config = require('./middle-end/config');
 const App = require('./App');
 
 it('renders without crashing.', () => {
 
-    M.initialize();
+    const { store, mods } = MiddleEnd.create(Config).initialize();
 
     const { getByText } = Testing.render(
         <App
-            store={M.store}
-            history={M.mods.router.history}
+            store={store}
+            history={mods.router.history}
         />
     );
 
--- src/middle-end/index.js
+++ src/middle-end/index.js
@@ -1,20 +1,4 @@
-const Redux = require('redux');
-const ReduxDevtools = require('redux-devtools-extension/logOnlyInProduction');
 const MiddleEnd = require('strange-middle-end');
+const Config = require('./config');
 
-module.exports = MiddleEnd.create({
-    mods: () => ({
-        counter: require('./counter'),
-        router: require('./router')
-    }),
-    createStore: (reducer, { router }) => {
-
-        return Redux.createStore(reducer, ReduxDevtools.composeWithDevTools(
-            Redux.applyMiddleware(
-                MiddleEnd.middleware.thunk,
-                MiddleEnd.middleware.errorLogger,    // TODO only in debug mode
-                router.middleware
-            )
-        ));
-    }
-});
+module.exports = MiddleEnd.create(Config);
