044b0e376e1297278da21e1d70ab4e28b01e6db3
wswoodruff <williamswoodruff@gmail.com>
2019-10-11 14:08:46 -0400

    Remove package description, upgrade eslint-plugin-react, bypass eslint bug by declaring var

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,12 @@ const { default: Styled } = require('styled-components');
 
 const internals = {};
 
+// NOTE: This used to be written directly in the component with curly braces
+// but there is a bug in eslint-plugin-react that says {' · '} fails linting
+// even though it is valid
+// Follow the issue's progress here https://github.com/yannickcr/eslint-plugin-react/issues/2454
+const divider = ' · ';
+
 module.exports = () => {
 
     const { Link } = internals;
@@ -12,7 +18,7 @@ module.exports = () => {
         <div>
             <h1>Strangeluv</h1>
             <Link exact to='/'>Home</Link>
-            {' · '}
+            {divider}
             <Link to='/counter'>Counter</Link>
         </div>
     );
