4f8c0416693477f3d130d34a2a172e52fb01b9cb
Tyler Constance <tyler@tylerconstance.com>
2019-10-18 13:44:34 -0400

    Deleted accidental addition

--- src/components/GlobalStyle.js
+++
@@ -1,9 +0,0 @@
-const Styled = require('styled-components');
-const NormalizeMixin = require('polished/lib/mixins/normalize');
-
-module.exports = Styled.createGlobalStyle`
-    ${NormalizeMixin()}
-    body {
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-    }
-`;
