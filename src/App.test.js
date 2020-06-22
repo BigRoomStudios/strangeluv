a82984205a8f0f683cda6a2ef41e8976385206b3
Zack McCartney <zack@bigroomstudios.com>
2020-06-22 12:31:32 -0400

    Fix middle-end naming in test

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,11 @@ require('regenerator-runtime/runtime');
 const React = require('react');
 const Testing = require('@testing-library/react');
 const App = require('./App');
-const MiddleEnd = require('./middle-end');
+const M = require('./middle-end');
 
 it('renders without crashing.', () => {
 
-    const middleEnd = MiddleEnd
-        .create()
-        .initialize();
+    const middleEnd = M.create().initialize();
 
     const { getByText } = Testing.render(
         <App
