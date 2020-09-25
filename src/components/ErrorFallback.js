bceeb392205a0256a70a8dd532149f7f34e82715
wswoodruff <williamswoodruff@gmail.com>
2020-09-25 02:03:44 -0400

    Lint

--- src/components/ErrorFallback.js
+++ src/components/ErrorFallback.js
@@ -2,12 +2,14 @@ const React = require('react');
 
 module.exports = () => {
 
+    const handleClick = () => window.location.reload();
+
     return (
         <>
             <h3>Oops! Something went wrong.</h3>
             <h4>
                 You can keep browsing or try{' '}
-                <button onClick={() => window.location.reload()}>
+                <button onClick={handleClick}>
                     reloading the page
                 </button>.
             </h4>
